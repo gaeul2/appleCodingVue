ff9993be3174ccca92564ba24c08db4b305b9917
gaeul <e0540@hanmail.net>
2025-08-29 00:34:59 +0900

    리액트보다 쉬운 Vue 반복문 v-for

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
-  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  <div>
-    원룸샵
-    <h4 :class="클래스" :style="스타일">{{ products[0].title }}</h4>
-<!--    <p>{{ price1 }} 만원</p>-->
-    <p>{{ products[0].price }} 만원</p>
+  <div class="menu">
+    <a v-for="(menu, i) in 메뉴들" :key="menu">{{i+1}}번째 메뉴 : {{ menu }}</a>
   </div>
-  <div>
-    <h4>{{ products[1].title }}</h4>
-<!--    <p>{{ price2 }} 만원</p>-->
-    <p>{{ products[1].price }} 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[2].title }}</h4>
-    <p>{{ price3 }} 만원</p>
+  원룸샵
+  <div v-for="title in products" :key="title">
+    <h4>{{title.title}}</h4>
+    <p :style="스타일">{{title.price}} <span :style="검정">만원</span></p>
   </div>
 </template>
 
@@ -25,9 +17,11 @@ export default {
       price1 : 60,
       price2 : 70,
       스타일 : 'color : blue',
+      검정 : 'color:black',
       클래스 : 'test-class',
       products : [ {'title' : '역삼동원룸', 'price' : 60}, {'title' :'천호동원룸', 'price' : 70}, {'title' :'마포구원룸', 'price' : 80}],
       price3: 80,
+      메뉴들 : ['Home', 'Shop', 'About']
     }
   },
   components : {
@@ -42,5 +36,15 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align:center;
     color:#2c3e50;
+  }
+
+  .menu{
+    background : darkslateblue;
+    padding : 15px;
+    border-radius: 5px;
+  }
+  .menu a{
+    color : white;
+    padding : 10px;
   }
 </style>
